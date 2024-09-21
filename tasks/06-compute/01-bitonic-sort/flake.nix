78bd0748404cba8939405d592eb94fc890c25844
Sergei Zimmerman <52955378+serjzimmerman@users.noreply.github.com>
2024-09-22 00:10:39 +0300

    chore(flake): bump inputs and drop custom opencl-hpp

--- tasks/06-compute/01-bitonic-sort/flake.nix
+++ tasks/06-compute/01-bitonic-sort/flake.nix
@@ -10,57 +10,50 @@
     flake-utils,
     ...
   } @ inputs: let
-    systems = ["x86_64-linux" "aarch64-linux"];
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
   in
-    flake-utils.lib.eachSystem systems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [
-          (self: super: {
-            opencl-clhpp = super.opencl-clhpp.overrideAttrs (old: rec {
-              version = "2023.02.06";
-              src = self.fetchFromGitHub {
-                owner = "KhronosGroup";
-                repo = "OpenCL-CLHPP";
-                rev = "v${version}";
-                sha256 = "sha256-QOZnPc0WAdI1ZSH8rVErEkWDLxoPdzGqfwqPLa8qe9Q=";
-              };
-            });
-          })
-        ];
-      };
-
-      clang-tools = pkgs.clang-tools_17;
-
-      devShell =
-        (pkgs.mkShell.override {
-          stdenv = pkgs.llvmPackages_17.stdenv;
-        }) {
-          nativeBuildInputs = with pkgs; [
-            cmake
-            python311
-            ninja
-            clang-tools
-            gtest
-            boost
-            flex
-            bison
-            just
-            range-v3
-            fmt
-            llvmPackages_17.openmp
-            opencl-clhpp
-            opencl-info
-            opencl-headers
-            ocl-icd
-            eigen
-          ];
+    flake-utils.lib.eachSystem systems (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
         };
-    in {
-      devShells = {
-        default = devShell;
-      };
 
-      formatter = pkgs.alejandra;
-    });
+        clang-tools = pkgs.clang-tools_17;
+
+        devShell =
+          (pkgs.mkShell.override {
+            stdenv = pkgs.llvmPackages_17.stdenv;
+          })
+          {
+            nativeBuildInputs = with pkgs; [
+              cmake
+              python311
+              ninja
+              clang-tools
+              gtest
+              boost
+              flex
+              bison
+              just
+              range-v3
+              fmt
+              llvmPackages_17.openmp
+              opencl-clhpp
+              clinfo
+              opencl-headers
+              ocl-icd
+              eigen
+            ];
+          };
+      in {
+        devShells = {
+          default = devShell;
+        };
+
+        formatter = pkgs.alejandra;
+      }
+    );
 }
